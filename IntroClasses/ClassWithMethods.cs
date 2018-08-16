2a3fe242260c9bca4cfa02e1c4ff51110537e5e2
myasinK <yasinul@gmail.com>
2018-08-15 18:16:15 -0700

    Finish drill on classes and methods

---
+++ IntroClasses/ClassWithMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroClasses
+{
+    class ClassWithMethods
+    {
+        public static int Add10(int x)
+        {
+            return x + 10;
+        }
+        public static int Minus10(int x)
+        {
+            return x - 10;
+        }
+        public static int Mult10(int x)
+        {
+            return x * 10;
+        }
+    }
+}
---
+++ IntroClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            int y = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine(ClassWithMethods.Add10(y));
+            Console.WriteLine(ClassWithMethods.Minus10(y));
+            Console.WriteLine(ClassWithMethods.Mult10(y));
+            Console.ReadLine();
+        }  
+    }
+}
